cba04bd3d67dd2fad7fdcbafc1f80754ff079a0b
leejungeun <feversore@gmail.com>
2021-10-14 20:38:10 +0900

    아티스트 리스트

---
+++ js/ui.js
@@ -0,0 +1,20 @@
+$(window).load(function(){
+    var $grid = $('.grid_wrapper');
+    $grid.masonry({
+        itemSelector: '.grid_item',
+        isAnimated:true,
+        horizontalOrder:true
+    });
+});
+
+function initLikedBtn(self){
+   // console.log('aaaa')
+   var $this = $(self);
+   var likedBtn = $('.btn_like');
+
+   if($this.hasClass('on') == false){
+        $this.addClass('on').append("<span class='blind'>좋아요</span>");
+   } else {
+        $this.removeClass('on').find('span').remove();
+   }
+}
